8772ea0f87e06eaf69179a1573884b532094a64c
ayomide-adekunle <mailayomide@gmail.com>
2019-10-26 00:13:22 -0400

    update

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor() {
+    let map_url = 'https://www.google.com/maps/dir/University+of+Michigan-Flint,+303+E+Kearsley+St,+Flint,+MI+48502/43.0315969,-83.6700995/@43.0237623,-83.6787598,2584m/data=!3m1!1e3!4m8!4m7!1m5!1m1!1s0x88238217c25d9827:0xf5cdab96dcc98e0c!2m2!1d-83.6894611!2d43.0194791!1m0!5m2!1e4!1e1'
+
+  }
 
 }
